7c2fa68debdf99f773a9eb5890f1acc898bb6b4c
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-12-08 12:27:23 -0800

    Add pattern matching on Match operator against generic object types

---
+++ src/AllenNeuralDynamics.Behavior.Distributions/MatchDistribution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive;
+
+namespace AllenNeuralDynamics.Behavior.Distributions
+{
+    /// <summary>
+    /// Partial class for MatchDistribution to eager deserialization behavior.
+    /// </summary>
+    partial class MatchDistribution
+    {
+        private static System.IObservable<TResult> Process<TResult>(System.IObservable<object> source)
+                   where TResult : Distribution
+        {
+            return System.Reactive.Linq.Observable.Create<TResult>(observer =>
+            {
+                var sourceObserver = System.Reactive.Observer.Create<object>(
+                    value =>
+                    {
+                        var match = value as TResult;
+                        if (match != null) observer.OnNext(match);
+                    },
+                    observer.OnError,
+                    observer.OnCompleted);
+                return System.ObservableExtensions.SubscribeSafe(source, sourceObserver);
+            });
+        }
+    }
+}
+
